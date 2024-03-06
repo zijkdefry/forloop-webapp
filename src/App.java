52d41e3a44338ebacddfad6ae300f295ca952c4e
zijkdefry <tanivan2u@gmail.com>
2024-03-07 00:13:08 +0800

    unimplement external closing of server

--- src/App.java
+++ src/App.java
@@ -2,7 +2,6 @@ public class App {
     public static void main(String[] args) throws Exception {
         var resources = new Item[] {
             new Item("index.html", "text/html"),
-            new Item("ascript.js", "text/javascript"),
             new Item("style.css", "text/css")
         };
         
--- src/WebServer.java
+++ src/WebServer.java
@@ -9,33 +9,19 @@ public class WebServer {
         return httpRequest.replaceAll("GET /", "").replaceAll(" HTTP/1.1", "");
     }
 
-    private static void ServeNotFount(PrintStream out, String notFoundResource) {
+    private static void serveNotFount(PrintStream out, String notFoundResource) {
         out.println("HTTP/1.1 404 Not Found");
         out.println("Content-Type: text");
         out.println();
         out.printf("404 Not Found: %s does not exist\n", notFoundResource);
     }
 
-    private static void ServeClose(PrintStream out) {
-        out.println("HTTP/1.1 204 No Content");
-        out.println();
-    }
-
-    // Returns running state of the server
-    private static boolean Serve(String req, PrintStream out) {
+    private static void serve(String req, PrintStream out) {
         var resource = getPath(req);
 
         if (resource.isEmpty()) resource = "index.html";
 
-        if (resource.equals("close")) {
-            ServeClose(out);
-            return false;
-        }
-
-        if (!Resources.has(resource)) {
-            ServeNotFount(out, resource);
-            return true;
-        }
+        if (!Resources.has(resource)) serveNotFount(out, resource);
         
         var item = Resources.get(resource);
 
@@ -43,27 +29,25 @@ public class WebServer {
         out.printf("Content-Type: %s\n", item.type);
         out.println();
         out.println(item.nameContent);
-        return true;
     }
 
+    private static ServerSocket server;
+
     public static void run() throws IOException {
-        var server = new ServerSocket(8080, 5);
-        
-        var running = true;
-        while (running) {
+        server = new ServerSocket(8080, 5);
+
+        while (true) {
             var client = server.accept();
 
             var input = new BufferedReader(new InputStreamReader(client.getInputStream()));
             var req = input.readLine();
 
             var output = new PrintStream(client.getOutputStream());
-            running = Serve(req, output);
+            serve(req, output);
 
             output.close();
             input.close();
             client.close();
         }
-
-        server.close();
     }
 }
